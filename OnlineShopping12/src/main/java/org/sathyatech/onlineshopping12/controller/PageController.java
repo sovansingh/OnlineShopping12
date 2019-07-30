b0c64dfb6aaaf9c94aea71ac1fe0117148b37901
sovan singh <ssovan35@gmail.com>
2019-07-30 09:07:14 +0530

    completed the interface theme and other issues!

--- OnlineShopping12/src/main/java/org/sathyatech/onlineshopping12/controller/PageController.java
+++ OnlineShopping12/src/main/java/org/sathyatech/onlineshopping12/controller/PageController.java
@@ -10,6 +10,23 @@ public class PageController {
 	@RequestMapping(value= {"/","home","index"})
 	public ModelAndView index() {
 		ModelAndView mav=new ModelAndView("page");
+		mav.addObject("title", "Home");
+		mav.addObject("userClickHome", true);
+		return mav;
+	}
+	
+	@RequestMapping(value= "/about")
+	public ModelAndView about() {
+		ModelAndView mav=new ModelAndView("page");
+		mav.addObject("title", "About Us");
+		mav.addObject("userClickAbout", true);
+		return mav;
+	}
+	@RequestMapping(value= "/contact")
+	public ModelAndView contact() {
+		ModelAndView mav=new ModelAndView("page");
+		mav.addObject("title", "Contact Us");
+		mav.addObject("userClickContact", true);
 		return mav;
 	}
 }
